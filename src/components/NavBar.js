4f2fd129bfef9dd9607d2e3766d8e2a1287e9690
Ethan Briscoe <ethanlbriscoe@gmail.com>
2024-07-17 07:22:21 -0500

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -36,7 +36,7 @@ export const NavBar = () => {
     <Router>
       <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
         <Container>
-          <Navbar.Brand href="/">
+          <Navbar.Brand href="https://ethanbriscoe25.github.io/personal-interactive-portfolio/">
             <img src={logo} alt="Logo" />
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav">
